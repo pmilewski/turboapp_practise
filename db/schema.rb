84b1365f91bfdd34f78c7c902329e340e1a5a1c6
Piotr Milewski <milewski.piotr@gmail.com>
2025-09-16 02:53:51 +0200

    posts resource with turbomodal

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_15_162809) do
+ActiveRecord::Schema[8.0].define(version: 2025_09_16_000039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -22,6 +22,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_15_162809) do
 
   create_table "movies", force: :cascade do |t|
     t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
