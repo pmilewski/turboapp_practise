87a80b0fb8f0d1284c135be8d3aa76b4662b5567
Piotr Milewski <milewski.piotr@gmail.com>
2025-09-23 17:34:38 +0200

    install sortablejs

---
+++ app/javascript/controllers/sortable_controller.js
@@ -0,0 +1,11 @@
+import { Controller } from "@hotwired/stimulus"
+import Sortable from "sortablejs"
+
+// Connects to data-controller="sortable"
+export default class extends Controller {
+  connect() {
+    Sortable.create(this.element, {})
+  }
+}
+
+
